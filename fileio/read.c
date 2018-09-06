ca04423a203e0da765c8e5d767cbbe02003ad986
Shishir Dey <shishirdey241097@gmail.com>
2018-09-06 19:36:10 +0530

    1.0

---
+++ fileio/read.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/* REGULAR INCLUDES */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: ./read filename.txt\n");
+		return 1;
+	}
+	int fd = open(argv[1], O_RDONLY);
+	if (fd == -1) {
+		fprintf(stderr, "Unable to open file %s\n", argv[1]);
+		return 1;
+	}
+	char *read_data = (char *)malloc(20 * sizeof(char));
+	char *read_data2 = read_data;
+	ssize_t r = read(fd, read_data, 20 * sizeof(char));
+	if (r == -1) {
+		fprintf(stderr, "Unable to read file %s\n", argv[1]);
+		return 1;
+	}
+	while (*read_data) {
+		fprintf(stdout, "%c", *read_data);
+		read_data++;
+	}
+	free(read_data2);
+	return 0;
+}
---
+++ fileio/write.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/* REGULAR INCLUDES */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 4) {
+		fprintf(stderr, "Usage: ./write filename.txt hello N\n");
+		return 1;
+	}
+	int fd = creat(argv[1], S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd == -1) {
+		fprintf(stderr, "Unable to create file %s\n", argv[1]);
+		return 1;
+	}
+	write(fd, argv[2], atoi(argv[3]));
+	return 0;
+}
